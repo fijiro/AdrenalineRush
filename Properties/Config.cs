ef047ac2a5ab3a16c50c926df68341ac3e8c1336
fijiro <jiro.protonmail@protonmail.com>
2023-06-11 00:29:00 +0300

    v1.0.0

--- Properties/Config.cs
+++ Properties/Config.cs
@@ -5,20 +5,27 @@ namespace Adrenaline.Configs
     internal class AdrenalineConfig
     {
         public static ConfigEntry<bool> EnableMod { get; private set; }
-        public static ConfigEntry<int> AdrenalineDuration { get; private set; }
+        public static ConfigEntry<float> AdrenalineDuration { get; private set; }
+		public static ConfigEntry<float> AdrenalineCooldown { get; private set; }
 
-        public static void Init(ConfigFile Config)
+		public static void Init(ConfigFile Config)
         {
             string debugmode = "General Settings";
+
             EnableMod = Config.Bind(debugmode, "Enable Mod", true,
                 new ConfigDescription("Enable or disable the mod",
                 null,
                 new ConfigurationManagerAttributes { IsAdvanced = false, Order = 100 }));
 
-            AdrenalineDuration = Config.Bind(debugmode, "Adrenaline Duration", 10,
+            AdrenalineDuration = Config.Bind(debugmode, "Adrenaline Duration", 10f,
                 new ConfigDescription("Duration of the painkiller buff in seconds. Doesn't require restart",
-                new AcceptableValueRange<int>(0, 120),
+                new AcceptableValueRange<float>(0f, 120f),
                 new ConfigurationManagerAttributes { IsAdvanced = false, Order = 50 }));
-        }
+
+			AdrenalineCooldown = Config.Bind(debugmode, "Adrenaline Cooldown", 120f,
+				new ConfigDescription("Duration of the painkiller buff in seconds. Doesn't require restart",
+				new AcceptableValueRange<float>(60f, 300f),
+				new ConfigurationManagerAttributes { IsAdvanced = false, Order = 00 }));
+		}
     }
 }
