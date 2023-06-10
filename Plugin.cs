0338e340765df35abb68ffe6055439a6c05b85e1
fijiro <jiro.protonmail@protonmail.com>
2023-06-10 17:25:33 +0300

    1.0.0

--- Plugin.cs
+++ Plugin.cs
@@ -3,17 +3,15 @@ using Adrenaline.Configs;
 
 namespace Adrenaline
 {
-
-    //create BepInEx plugin and call the plugin
-    [BepInPlugin("com.kobrakon.adrenaline", "Adrenaline", "1.0.0")]
+    //todo: voiceline when adrenaline activates, cooldown?
+    //create BepInEx plugin, which starts the config and patch
+    [BepInPlugin("com.jiro.adrenalinerush", "AdrenalineRush", "1.0.0")]
     public class AdrenalinePlugin : BaseUnityPlugin
     {
         void Awake()
         {
             AdrenalineConfig.Init(Config);
             new AdrenalinePatch().Enable();
-            Logger.LogInfo("Adrenaline Config value: " + AdrenalineConfig.AdrenalineDuration.Value.ToString());
-            Logger.LogInfo("Adrenaline Default value: " + AdrenalineConfig.AdrenalineDuration.DefaultValue.ToString());
         }
     }
 }
--- Properties/Config.cs
+++ Properties/Config.cs
@@ -5,19 +5,19 @@ namespace Adrenaline.Configs
     internal class AdrenalineConfig
     {
         public static ConfigEntry<bool> EnableMod { get; private set; }
-        public static ConfigEntry<float> AdrenalineDuration { get; private set; }
+        public static ConfigEntry<int> AdrenalineDuration { get; private set; }
 
         public static void Init(ConfigFile Config)
         {
             string debugmode = "General Settings";
-            EnableMod = Config.Bind(debugmode, "Mod Enabled", true,
+            EnableMod = Config.Bind(debugmode, "Enable Mod", true,
                 new ConfigDescription("Enable or disable the mod",
                 null,
                 new ConfigurationManagerAttributes { IsAdvanced = false, Order = 100 }));
 
-            AdrenalineDuration = Config.Bind(debugmode, "Adrenaline Duration", 30f,
+            AdrenalineDuration = Config.Bind(debugmode, "Adrenaline Duration", 10,
                 new ConfigDescription("Duration of the painkiller buff in seconds. Doesn't require restart",
-                new AcceptableValueRange<float>(0f, 120f),
+                new AcceptableValueRange<int>(0, 120),
                 new ConfigurationManagerAttributes { IsAdvanced = false, Order = 50 }));
         }
     }
