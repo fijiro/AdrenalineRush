da65e5bc895f43a11cf00bc777fd2b5fd9fa566a
fijiro <jiro.protonmail@protonmail.com>
2023-06-10 18:55:06 +0300

    v1.0.0

--- AdrenalineRush.cs
+++ AdrenalineRush.cs
@@ -3,26 +3,26 @@ using System;
 using System.Linq;
 using System.Reflection;
 using Aki.Reflection.Patching;
-using static Adrenaline.Configs.AdrenalineConfig;
+using Adrenaline.Configs;
 
 namespace Adrenaline
 {
 	public class AdrenalinePatch : ModulePatch
 	{
+		//i have no clue what this does :D
 		static MethodInfo effectMethod = typeof(ActiveHealthControllerClass).GetMethods(BindingFlags.Instance | BindingFlags.NonPublic).First(m =>
 			m.GetParameters().Length == 6
 			&& m.GetParameters()[0].Name == "bodyPart"
 			&& m.GetParameters()[5].Name == "initCallback"
 			&& m.IsGenericMethod
 		);
+		//or this
 		protected override MethodBase GetTargetMethod() => typeof(Player).GetMethod("ReceiveDamage", BindingFlags.Instance | BindingFlags.NonPublic);
 
 		[PatchPostfix]
 		static void Postfix(ref Player __instance, EDamageType type)
 		{
-			if (EnableMod.Value == false) return;
-
-			float duration = AdrenalineDuration.Value;
+			if (AdrenalineConfig.EnableMod.Value == false) return;
 
 			if (type == EDamageType.Bullet || type == EDamageType.Explosion || type == EDamageType.Sniper || type == EDamageType.Landmine || type == EDamageType.GrenadeFragment)
 			{
@@ -34,8 +34,9 @@ namespace Adrenaline
 						return;
 					}
 
-					//here the actual painkiller effect is created
-					duration = AdrenalineDuration.Value;
+					//here the actual painkiller effect is created. Cooldown when?
+					int duration = AdrenalineConfig.AdrenalineDuration.Value;
+					
 					effectMethod.MakeGenericMethod(typeof(ActiveHealthControllerClass).GetNestedType("PainKiller", BindingFlags.Instance | BindingFlags.NonPublic)).Invoke(__instance.ActiveHealthController, new object[] { EBodyPart.Head, 0f, duration, 5f, 1f, null });
 
 				}
